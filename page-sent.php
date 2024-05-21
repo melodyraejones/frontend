37706af09f01a8f727e526194165de6f7231c268
Akshay sharma <akshaysharma5432@gmail.com>
2024-05-21 15:11:57 -0400

    set up contact confirmation page

---
+++ page-sent.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Sent Page
+*/
+get_header();
+?>
+<section class="section-thank">
+<div class="cta-image-box-thankyou" role="img" aria-label="Woman meditating"></div>
+<h2 class="thankyou-message">Thank you for your message.</h2>
+<p class="thankyou-info">We will contact you soon.</p>
+<p class="program-navigation">In the meantime,  explore our <a href="<?php echo home_url(); ?>">programs</a> and find the one that best suits your needs.</p>
+
+</section>
+<?
+get_footer(); 
+?>
